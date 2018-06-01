1c2ff65dbb69599ca430b6230580f7ca5f325e07
Priyank Upadhyay <priyank@platinos.in>
2018-06-01 22:55:00 +0530

    qwerty

--- app.js
+++ app.js
@@ -19,10 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
-  req.db = db;
-  next();
-});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
