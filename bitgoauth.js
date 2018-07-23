fa0165ebe2395bb2b34db383952774abcc5bde82
kumar rahul <rahul.sinha.raw@gmail.com>
2018-07-24 00:13:32 +0530

    hgfghfgh

--- bitgoauth.js
+++ bitgoauth.js
@@ -1,5 +1,5 @@
 var BitGo = require('bitgo');
-const token =  'v2x0df1d25b47c8576cc9df96ee2fe65f682ece33a61cdc422bf609085d8e8ea48e'
+const token =  'v2x01cb852266aff3350a36660b2e3edd0d81d0848f37fd2eb4ff99e5792412ee52'
 var bitgo = new BitGo.BitGo({ accessToken:token}); // defaults to testnet. add env: 'prod' if you want to go against mainnet
 let coin = bitgo.coin('tbtc');
 module.exports=bitgo;
