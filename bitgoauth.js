6d794ca6ef6d136dc43211d05e9fb05868885fe7
Priyank Upadhyay <priyank@platinos.in>
2018-08-26 00:07:34 +0530

    qwerty

--- bitgoauth.js
+++ bitgoauth.js
@@ -1,7 +1,7 @@
 var BitGo = require('bitgo');
 
-const token = 'v2x077634d02fa892b425ad83d4ea1e78ce99a63136b2ee317dbe26821cdcbc46cb'
-var bitgo = new BitGo.BitGo({ accessToken:token}); // defaults to testnet. add env: 'prod' if you want to go against mainnet
+const token = 'v2x93895f66d34eaa9daebf37ec07a039d84f98828276602727707d40401097ea33'
+var bitgo = new BitGo.BitGo({ accessToken: token, env: 'prod'}); // defaults to testnet. add env: 'prod' if you want to go against mainnet
 let coin = bitgo.coin('tbtc');
 bitgo.unlock({otp:'0000000'}).then(function(unlockRes){
 console.log(unlockRes);
