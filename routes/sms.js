099d2b5a5f6b041d2e86b979a6c047d0aff5041e
Priyank Upadhyay <priyank@platinos.in>
2018-09-15 02:43:40 +0530

    qwert

--- routes/sms.js
+++ routes/sms.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-router.get('/:phoneNo', function (request, response, next) {
+router.get('/:phoneNo', function (request1, response, next) {
     res.setHeader('Content-Type', 'application/json');
 
     var options = {
         "method": "POST",
         "hostname": "control.msg91.com",
         "port": null,
-        "path": "/api/sendotp.php?template=&otp_length=6&authkey=147351AegMLMB0q58e0e449&message=&sender=ELZIRE&mobile=" + request.params.phoneNo,
+        "path": "/api/sendotp.php?template=&otp_length=6&authkey=147351AegMLMB0q58e0e449&message=&sender=ELZIRE&mobile=" + request1.params.phoneNo,
         "headers": {}
     };
 
@@ -22,7 +22,7 @@ router.get('/:phoneNo', function (request, response, next) {
         res.on("end", function () {
             var body = Buffer.concat(chunks);
             console.log(body.toString());
-            resonse.send(JSON.stringify({ "status": 200, "error": null, "response": body }));
+            resonse.send(JSON.stringify({ "status": 200, "error": null, "response": body.toString() }));
         });
     });
 
